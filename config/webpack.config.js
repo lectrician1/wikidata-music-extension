0a02f1117da09cd89b4bb8f137fb4da32933e4f2
lectrician1 <jayandseth@gmail.com>
2022-06-21 20:59:28 -0500

    poggers!

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const { merge } = require('webpack-merge');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const webpack = require('webpack');
 
 const common = require('./webpack.common.js');
 const PATHS = require('./paths');
@@ -9,11 +11,23 @@ const PATHS = require('./paths');
 const config = (env, argv) =>
   merge(common, {
     entry: {
-      popup: PATHS.src + '/popup.js',
       contentScript: PATHS.src + '/contentScript.js',
-      background: PATHS.src + '/background.js',
     },
     devtool: argv.mode === 'production' ? false : 'source-map',
+    plugins: [
+      new NodePolyfillPlugin(),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+      }),
+    ],
+    node: undefined,
+    resolve: {
+      alias: {
+        stream: require.resolve('stream-browserify'),
+        zlib: require.resolve('browserify-zlib'),
+      },
+    },
   });
 
 module.exports = config;
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,43 +1,126 @@
-'use strict';
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
 
-// Content script file will run in the context of web page.
-// With content script you can manipulate the web pages using
-// Document Object Model (DOM).
-// You can also pass information to the parent extension.
+var Discogs = require('disconnect').Client;
 
-// We execute this script by making an entry in manifest.json file
-// under `content_scripts` property
-
-// For more information on Content Scripts,
-// See https://developer.chrome.com/extensions/content_scripts
-
-// Log `title` of current active web page
-const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
-console.log(
-  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
-);
-
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  (response) => {
-    console.log(response.message);
-  }
-);
-
-// Listen for message
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'COUNT') {
-    console.log(`Current count is ${request.payload.count}`);
-  }
-
-  // Send an empty response
-  // See https://github.com/mozilla/webextension-polyfill/issues/130#issuecomment-531531890
-  sendResponse({});
-  return true;
+const WBK = require('wikibase-sdk');
+const wdk = WBK({
+  instance: 'https://www.wikidata.org',
+  sparqlEndpoint: 'https://query.wikidata.org/sparql',
 });
+
+const property = 'P1954';
+
+function getEntityLabel(id) {
+  var label;
+  var apiurl = wdk.getEntities({
+    ids: [id],
+    language: 'en',
+  });
+  fetch(apiurl)
+    .then((response) => response.json)
+    .then(wdk.parse.wb.entities)
+    .then((d) => {
+      var entity = d[0];
+      label = entity.labels['en'].value;
+    });
+
+  return label;
+}
+
+function appendDiscogsValue(wikidataProp, discogsProp, value) {
+  $(`#${wikidataProp}`)
+    .find('.wikibase-statementgroupview-property-label')
+    .append(`<br>Discogs ${discogsProp}:<br>${value}`);
+}
+
+if ($('#' + property).length) {
+  // Get qid
+  var entityurl = new URL(window.location.href);
+  var q = entityurl.pathname.substring(6);
+
+  var apiurl = wdk.getEntities({
+    ids: [q],
+  });
+  fetch(apiurl)
+    .then((response) => response.json())
+    // Turns the response in an array of simplified entities
+    .then(wdk.parse.wb.entities)
+    .then((d) => {
+      var e = d[q];
+      var masterid = parseInt(e.claims[property][0]);
+
+      var db = new Discogs().database();
+      db.getMaster(masterid, function (err, data) {
+        $(`#${property}`)
+          .find('.wikibase-snakview-body')
+          .eq(0)
+          .append(
+            $(
+              '<a id="discogs-extra">Show extra Discogs data</a><div id="extra-expanded" style="display: none"></div>'
+            )
+          );
+
+        for (const discogsProp in data) {
+          var wikidataProp;
+          var notfound = false;
+
+          // Get discogs value in future and parse
+          switch (discogsProp) {
+            case 'genres':
+              wikidataProp = 'P136';
+              break;
+            case 'styles':
+              wikidataProp = 'P136';
+              break;
+            case 'artists':
+              wikidataProp = 'P175';
+              break;
+            case 'tracklist':
+              wikidataProp = 'P658';
+              break;
+            case 'title':
+              wikidataProp = 'P1476';
+              break;
+            case 'year':
+              wikidataProp = 'P577';
+              break;
+            default:
+              notfound = true;
+          }
+
+          var discogsValue = JSON.stringify(data[discogsProp], null, 4);
+
+          if (!notfound) {
+            // if discogs has data wikidata has, append data to wikidata element
+            if (wikidataProp in e.claims) {
+              appendDiscogsValue(wikidataProp, discogsProp, discogsValue);
+            }
+            // if discogs has data wikidata does not, add a statement
+            else {
+              var formattedDiscogsValue = discogsValue.replace(
+                /\n( *)/g,
+                function (match, p1) {
+                  return '<br>' + '&nbsp;'.repeat(p1.length);
+                }
+              );
+              $('.wikibase-listview')
+                .eq(0)
+                .append(
+                  `Discogs ${discogsProp}<pre>${formattedDiscogsValue}</pre><br>`
+                );
+            }
+          } else {
+            $('#extra-expanded').append(
+              `Discogs ${discogsProp}<br><pre>${discogsValue}</pre>`
+            );
+          }
+        }
+
+        $('#discogs-extra').on('click', function () {
+          $('#extra-expanded').slideToggle('fast');
+        });
+      });
+    });
+}
