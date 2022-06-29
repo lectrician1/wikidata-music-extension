c58901a433f1f10c9514e60ddc1040b262abb8ff
lectrician1 <jayandseth@gmail.com>
2022-06-28 21:05:58 -0500

    yay almost

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
+require('jquery-sendkeys');
 var Discogs = require('disconnect').Client;
 
 const WBK = require('wikibase-sdk');
@@ -35,92 +36,109 @@ function appendDiscogsValue(wikidataProp, discogsProp, value) {
     .append(`<br>Discogs ${discogsProp}:<br>${value}`);
 }
 
-if ($('#' + property).length) {
-  // Get qid
-  var entityurl = new URL(window.location.href);
-  var q = entityurl.pathname.substring(6);
+jQuery(() => {
+  if ($('#' + property).length) {
+    // Get qid
+    var entityurl = new URL(window.location.href);
+    var qid = entityurl.pathname.substring(6);
 
-  var apiurl = wdk.getEntities({
-    ids: [q],
-  });
-  fetch(apiurl)
-    .then((response) => response.json())
-    // Turns the response in an array of simplified entities
-    .then(wdk.parse.wb.entities)
-    .then((d) => {
-      var e = d[q];
-      var masterid = parseInt(e.claims[property][0]);
+    var apiurl = wdk.getEntities({
+      ids: [qid],
+    });
+    fetch(apiurl)
+      .then((response) => response.json())
+      // Turns the response in an array of simplified entities
+      .then(wdk.parse.wb.entities)
+      .then((d) => {
+        var entity = d[qid];
+        var masterid = parseInt(entity.claims[property][0]);
 
-      var db = new Discogs().database();
-      db.getMaster(masterid, function (err, data) {
-        $(`#${property}`)
-          .find('.wikibase-snakview-body')
-          .eq(0)
-          .append(
-            $(
-              '<a id="discogs-extra">Show extra Discogs data</a><div id="extra-expanded" style="display: none"></div>'
-            )
-          );
-
-        for (const discogsProp in data) {
-          var wikidataProp;
-          var notfound = false;
-
-          // Get discogs value in future and parse
-          switch (discogsProp) {
-            case 'genres':
-              wikidataProp = 'P136';
-              break;
-            case 'styles':
-              wikidataProp = 'P136';
-              break;
-            case 'artists':
-              wikidataProp = 'P175';
-              break;
-            case 'tracklist':
-              wikidataProp = 'P658';
-              break;
-            case 'title':
-              wikidataProp = 'P1476';
-              break;
-            case 'year':
-              wikidataProp = 'P577';
-              break;
-            default:
-              notfound = true;
-          }
-
-          var discogsValue = JSON.stringify(data[discogsProp], null, 4);
-
-          if (!notfound) {
-            // if discogs has data wikidata has, append data to wikidata element
-            if (wikidataProp in e.claims) {
-              appendDiscogsValue(wikidataProp, discogsProp, discogsValue);
-            }
-            // if discogs has data wikidata does not, add a statement
-            else {
-              var formattedDiscogsValue = discogsValue.replace(
-                /\n( *)/g,
-                function (match, p1) {
-                  return '<br>' + '&nbsp;'.repeat(p1.length);
-                }
-              );
-              $('.wikibase-listview')
-                .eq(0)
-                .append(
-                  `Discogs ${discogsProp}<pre>${formattedDiscogsValue}</pre><br>`
-                );
-            }
-          } else {
-            $('#extra-expanded').append(
-              `Discogs ${discogsProp}<br><pre>${discogsValue}</pre>`
+        var db = new Discogs().database();
+        db.getMaster(masterid, function (err, data) {
+          $(`#${property}`)
+            .find('.wikibase-snakview-body')
+            .eq(0)
+            .append(
+              $(
+                '<a id="discogs-extra">Show extra Discogs data</a><div id="extra-expanded" style="display: none"></div>'
+              )
             );
-          }
-        }
 
-        $('#discogs-extra').on('click', function () {
-          $('#extra-expanded').slideToggle('fast');
+          for (const discogsProp in data) {
+            var wikidataProp;
+            var notfound = false;
+
+            // Get discogs value in future and parse
+            switch (discogsProp) {
+              case 'genres':
+                wikidataProp = 'P136';
+                break;
+              case 'styles':
+                wikidataProp = 'P136';
+                break;
+              case 'artists':
+                wikidataProp = 'P175';
+                break;
+              case 'tracklist':
+                wikidataProp = 'P658';
+                break;
+              case 'title':
+                wikidataProp = 'P1476';
+                break;
+              case 'year':
+                wikidataProp = 'P577';
+                break;
+              default:
+                notfound = true;
+            }
+
+            var discogsValue = JSON.stringify(data[discogsProp], null, 4);
+
+            if (!notfound) {
+              // if discogs has data wikidata has, append data to wikidata element
+              if (wikidataProp in entity.claims) {
+                appendDiscogsValue(wikidataProp, discogsProp, discogsValue);
+              }
+              // if discogs has data wikidata does not, add a statement
+              else {
+                var formattedDiscogsValue = discogsValue.replace(
+                  /\n( *)/g,
+                  function (match, p1) {
+                    return '<br>' + '&nbsp;'.repeat(p1.length);
+                  }
+                );
+                $('.wikibase-listview')
+                  .eq(0)
+                  .append(
+                    `Discogs ${discogsProp}<pre>${formattedDiscogsValue}</pre><br><a class="discogs-add">Add</a>`
+                  );
+
+                $(document).on('click', '.discogs-add', function () {
+                  $(`a[title='Add a new statement']`)[0].click()
+                  setTimeout(function () {
+                    $('.wikibase-snakview-property').children('.ui-suggester-input').sendkeys(wikidataProp)
+
+                    setTimeout(function () {
+                      $('.ui-ooMenu:not(.wikibase-entitysearch-list) > .ui-ooMenu-item').eq(0).trigger('mouseenter.ooMenu')
+                      $('.ui-ooMenu:not(.wikibase-entitysearch-list) > .ui-ooMenu-item').eq(0).trigger({type:'mousedown.ooMenu', which: 1})
+                    }, 1000)
+                  }, 1000)
+                  
+
+                });
+              }
+            } else {
+              $('#extra-expanded').append(
+                `Discogs ${discogsProp}<br><pre>${discogsValue}</pre>`
+              );
+            }
+          }
+
+          $('#discogs-extra').on('click', function () {
+            $('#extra-expanded').slideToggle('fast');
+          });
         });
       });
-    });
-}
+  }
+
+})
